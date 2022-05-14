248ba38442d6f9f4f09ed4d2cbcf02c0651bebc0
lelonS <35070147+lelonS@users.noreply.github.com>
2022-05-14 11:48:13 +0200

    Add step function

--- chaos_game.py
+++ chaos_game.py
@@ -1,3 +1,5 @@
+from ast import Break
+from tkinter.tix import MAX
 import pygame
 import random
 from math import cos, sin, pi
@@ -36,15 +38,37 @@ def reset_screen():
     global SCREEN
     global allow_repeat_point
     global mult
+    global step
     SCREEN.fill(BACKGROUND)
     # SCREEN.blit(bg, (0, 0))
     draw_points(points)
     draw_text("allow_repeat_point=" + str(allow_repeat_point), (0, 20))
     draw_text("mult=" + str(mult), (0, 40))
+    draw_text("step=" + str(step), (0, 60))
 
+def add_point():
+    global SCREEN
+    global MAX_LOOP
+    global points
+    global prev_point
+    global allow_repeat_point
+    global current_point
+    global mult
+    tries = 0
+    while tries < MAX_LOOP:
+        random_point = random.choice(points)
+        if (random_point != prev_point or allow_repeat_point):# and SCREEN.get_at(get_new_point(current_point, random_point, mult)) != (255, 255, 255, 255):
+            prev_point = random_point
+            current_point = get_new_point(current_point, random_point, mult)
+            # pygame.draw.rect(SCREEN, (255, 0, 0),
+            #                  (current_point[0], current_point[1], 1, 1))
+            SCREEN.set_at(current_point, (255, 0, 0))
+            return
+    print("error")
+        
 
 # CONSTANTS
-WIDTH = 900
+WIDTH = 800
 HEIGHT = WIDTH
 
 RADIUS = WIDTH / 2 if WIDTH <= HEIGHT else HEIGHT / 2
@@ -61,6 +85,7 @@ pygame.display.update()
 
 # Variables
 amount_points = 3
+step = False
 allow_repeat_point = True
 amount_per_frame = 200
 mult = 0.5
@@ -77,18 +102,12 @@ while running:
 
     added_points = 0
     tries = 0
-    while added_points < amount_per_frame:
+    while added_points < amount_per_frame and not step:
         tries += 1
         if tries > MAX_LOOP:
             break
-        random_point = random.choice(points)
-        if (random_point != prev_point or allow_repeat_point) and SCREEN.get_at(get_new_point(current_point, random_point, mult)) != (255, 255, 255, 255):
-            prev_point = random_point
-            current_point = get_new_point(current_point, random_point, mult)
-            # pygame.draw.rect(SCREEN, (255, 0, 0),
-            #                  (current_point[0], current_point[1], 1, 1))
-            SCREEN.set_at(current_point, (255, 0, 0))
-            added_points += 1
+        add_point()
+        added_points += 1
 
     # Get events
     for event in pygame.event.get():
@@ -100,11 +119,25 @@ while running:
             reset_screen()
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
             amount_points -= 1
+            if amount_points <= 0: amount_points = 1
             points = get_points(amount_points, (WIDTH/2, HEIGHT/2))
             reset_screen()
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
             allow_repeat_point = not allow_repeat_point
             reset_screen()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            mult += 0.1
+            reset_screen()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            mult -= 0.1
+            reset_screen()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+            step = not step
+            reset_screen()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_s and step:
+            for i in range(amount_per_frame):
+                add_point()
+
 
     pygame.display.update()
 # QUIT
